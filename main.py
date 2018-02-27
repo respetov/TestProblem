541f837425d65f8be226c4626b8f111d7c8eb015
Juan Arteaga Carmona <juan.arteaga41567@gmail.com>
2018-02-27 17:56:08 +0100

    Added String to Matrix

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #Main file
+def stringToMatrix(string, numColls):
+    numColls = 7
+    matrizfinal = []
+    aux = []
+    pos = 0
+    for i in string:
+        aux.append(i)
+        if pos == numColls:
+            matrizfinal.append(aux)
+            aux = []
+            pos = 0
+        pos +=1
+    return matrizfinal
